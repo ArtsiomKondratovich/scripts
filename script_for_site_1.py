ef3a467a4a07cf3e2862b9c7e90291f5940964b6
artsiom <a-kondratovich@mail.ru>
2022-10-17 15:54:26 +0300

    цикл! не забыть

--- script_for_site_1.py
+++ script_for_site_1.py
@@ -3,13 +3,6 @@ from geopy.geocoders import Nominatim
 from bs4 import BeautifulSoup
 import lxml
 
-session = HTMLSession()
-
-URL = "https://oriencoop.cl/sucursales.htm"
-
-response = session.get(url=URL)
-response.html.render(sleep=5)
-
 
 def name_to_coords(name: str) -> list:
     geolocator = Nominatim(user_agent="script_for_site_1")
@@ -17,16 +10,39 @@ def name_to_coords(name: str) -> list:
     return [location.latitude, location.longitude]
 
 
-list_of_departments = []  # list =[(links, name), ...]
+def get_all_departmets() -> list:
+    URL = "https://oriencoop.cl/sucursales.htm"
 
-departments = response.html.find(".sub-menu")
-soup_of_departments = BeautifulSoup(departments, 'lxml').find_all('a')
-print(soup_of_departments)
+    response = session.get(url=URL)
+    response.html.render(sleep=1)
+    list_of_departments = []  # list =[(links, name), ...]
+
+    departments = response.html.find(".sub-menu")
+    for i in departments:
+        soup = BeautifulSoup(i.html, 'lxml').find('a')
+        list_of_departments.append((soup.get('href'), soup.text))
+    return list_of_departments
 
 
+temp = [('/sucursales/79', 'Valparaíso'), ('/sucursales/127', 'Santiago'), ('/sucursales/165', 'San Fernando'),
+        ('/sucursales/167', 'Cauquenes'), ('/sucursales/208', 'Los Ángeles'), ('/sucursales/231', 'Chillán'),
+        ('/sucursales/267', 'Temuco'), ('/sucursales/312', 'Puerto Montt')]
 
 
-# data = response.html.find(".s-dato")
-# data = BeautifulSoup(data[0].html, 'lxml').find_all('p')
-# name = BeautifulSoup(data[0].html, 'lxml').find('h3')
+def get_data_of_department(list_of_all_departmets: list) -> str:
+    BASE_URL = 'https://oriencoop.cl'
+    for i in temp:
+        NAME = i[1]
+        response = session.get(url=f'{BASE_URL}{i[0]}')
+        response.html.render(sleep=5)
+        raw_data = response.html.find('.s-dato', first=True)
+        soup = BeautifulSoup(raw_data, 'lxml').find_all('p')
+        print(soup)
+    return '......'
 
+
+if __name__ == '__main__':
+    print('strat')
+    session = HTMLSession()
+    # print(get_all_departmets())
+    get_data_of_department(temp)
--- temp.py
+++ temp.py
@@ -1,12 +1,33 @@
-temp = """Valparaíso
-Dirección:
-Esmeralda 940 L.4 - Valparaíso
-Teléfono:
-71-2201096
-Agente:
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
 
-Horarios:
-Mañana: 08.50 a 14.10 hrs.
-Tarde: 15.00 a 17.10 horas (L a J) / Hasta 16.10 horas (V)"""
+session = HTMLSession()
+temp = [('/sucursales/79', 'Valparaíso')]
 
-print(temp.split('\n'))
+BASE_URL = 'https://oriencoop.cl'
+for i in temp:
+    NAME = i[1]
+    response = session.get(url=f'{BASE_URL}{i[0]}')
+    response.html.render()
+    raw_data = response.html.find('.s-dato', first=True)
+    soup = BeautifulSoup(raw_data.html, 'lxml').find_all('p')
+    for i in soup:
+        print(i)
+        print(i.strong.get_text(), i.span.get_text())
+        for j in i.span:
+            print(j)
+
+"""<p>
+<strong>Dirección:</strong><br/>
+<span>Esmeralda 940 L.4 - Valparaíso</span>
+</p>, <p>
+<strong>Teléfono:</strong><br/>
+<span>71-2201096</span>
+</p>, <p>
+<strong>Agente:</strong><br/>
+<span> </span>
+</p>, <p>
+<strong>Horarios:</strong><br/>
+<span><img src="/resources/img/li.png"/> Mañana: 08.50 a 14.10 hrs.</span><br/>
+<span><img src="/resources/img/li.png"/> Tarde: 15.00 a 17.10 horas (L a J) / Hasta 16.10 horas (V)</span>
+</p>"""
