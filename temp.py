d06d848bbdeada289f7fa787efc9541b12cf44c4
artsiom <a-kondratovich@mail.ru>
2022-10-17 16:08:53 +0300

    изм

--- temp.py
+++ temp.py
@@ -1,21 +1,27 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
+from script_for_site_1 import name_to_coords
+
 session = HTMLSession()
 temp = [('/sucursales/79', 'Valparaíso')]
 
 BASE_URL = 'https://oriencoop.cl'
-for i in temp:
-    NAME = i[1]
-    response = session.get(url=f'{BASE_URL}{i[0]}')
+for one_object_soup in temp:
+    result_dict_for_one = {
+        "address": str(address),
+        "latlaon": name_to_coords(address),
+        "name": one_object_soup[one_object_soup[1]],
+    }
+
+    response = session.get(url=f'{BASE_URL}{one_object_soup[0]}')
     response.html.render()
     raw_data = response.html.find('.s-dato', first=True)
     soup = BeautifulSoup(raw_data.html, 'lxml').find_all('p')
-    for i in soup:
-        print(i)
-        print(i.strong.get_text(), i.span.get_text())
-        for j in i.span:
-            print(j)
+    for one_object_soup in soup:
+        address = one_object_soup.span.get_text()
+        for include_object_in_one_object in one_object_soup.span:
+            print(include_object_in_one_object)
 
 """<p>
 <strong>Dirección:</strong><br/>
